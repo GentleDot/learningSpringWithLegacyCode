6972de4040ff50d3d833e9e386c65dd106ab9d02
gentledot <gentledot.wp@gmail.com>
2020-04-19 19:15:55 +0900

    코드로 배우는 스프링 웹 프로젝트 - 기본적인 기능의 게시물 관리 : domain 객체 생성 (Board)

---
+++ src/main/java/net/gentledot/springcodeproject/model/board/Board.java
@@ -0,0 +1,116 @@
+package net.gentledot.springcodeproject.model.board;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class Board {
+    private long bno;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDate regDate;
+    private long viewcnt;
+
+    public Board(long bno, String title, String content, String writer, LocalDate regDate, long viewcnt) {
+        checkArgument(StringUtils.isNotBlank(title), "게시물 제목은 빈 값이 될 수 없습니다.");
+        checkArgument(title.getBytes(StandardCharsets.UTF_8).length <= 200, "게시물 제목은 200bytes 내외로 작성 가능합니다.");
+        checkArgument(StringUtils.isNotBlank(writer), "게시물 제목은 빈 값이 될 수 없습니다.");
+        checkArgument(writer.getBytes(StandardCharsets.UTF_8).length <= 50, "게시물 제목은 200bytes 내외로 작성 가능합니다.");
+        checkArgument(viewcnt >= 0, "view count 값은 음수를 허용하지 않습니다.");
+
+
+        this.bno = bno;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.regDate = ObjectUtils.defaultIfNull(regDate, LocalDate.now()) ;
+        this.viewcnt = ObjectUtils.defaultIfNull(viewcnt, 0L);
+    }
+
+    public long getBno() {
+        return bno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public long getViewcnt() {
+        return viewcnt;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("bno", bno)
+                .append("title", title)
+                .append("content", content)
+                .append("writer", writer)
+                .append("regDate", regDate)
+                .append("viewcnt", viewcnt)
+                .toString();
+    }
+
+    public static final class Builder {
+        private long bno;
+        private String title;
+        private String content;
+        private String writer;
+        private LocalDate regDate;
+        private long viewcnt;
+
+        public Builder() {
+        }
+
+        public Builder(Board board){
+            this.bno = board.bno;
+            this.title = board.title;
+            this.content = board.content;
+            this.writer = board.writer;
+            this.regDate = board.regDate;
+            this.viewcnt = board.viewcnt;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder writer(String writer) {
+            this.writer = writer;
+            return this;
+        }
+
+        public Builder regDate(LocalDate regDate) {
+            this.regDate = regDate;
+            return this;
+        }
+
+        public Board build() {
+            return new Board(bno, title, content, writer, regDate, viewcnt);
+        }
+    }
+}
---
+++ src/test/java/net/gentledot/springcodeproject/model/board/BoardTest.java
@@ -0,0 +1,36 @@
+package net.gentledot.springcodeproject.model.board;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class BoardTest {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    @DisplayName("Board 객체 생성 테스트")
+    void createBoardTest(){
+        String title = "테스트타이틀";
+        String content = "안녕하세요. 테스트 내용입니다.";
+        String username = "testUser01";
+        Board newBoard = new Board.Builder()
+                .title(title)
+                .content(content)
+                .writer(username)
+                .build();
+
+        assertThat(newBoard.getBno(), is(0L));
+        assertThat(newBoard.getTitle(), is(title));
+        assertThat(newBoard.getContent(), is(content));
+        assertThat(newBoard.getWriter(), is(username));
+        assertThat(newBoard.getRegDate(), is(notNullValue()));
+        assertThat(newBoard.getViewcnt(), is(0L));
+
+        log.debug("생성된 객체 : {}", newBoard);
+    }
+}
