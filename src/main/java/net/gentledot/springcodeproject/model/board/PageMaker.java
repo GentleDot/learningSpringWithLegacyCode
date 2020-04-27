a6601f88a5faf48483eae2ad2c3f66a1142b7974
gentledot <gentledot.wp@gmail.com>
2020-04-27 22:15:13 +0900

    코드로 배우는 스프링 웹 프로젝트 - 기본적인 기능의 게시물 관리 : Controller 구현 (2/3)_paging 처리용 class 생성 (PageMaker.class)

---
+++ src/main/java/net/gentledot/springcodeproject/model/board/PageMaker.java
@@ -0,0 +1,87 @@
+package net.gentledot.springcodeproject.model.board;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class PageMaker {
+    private int totalCount;
+    private int startPage;
+    private int endPage;
+    private boolean prev;
+    private boolean next;
+    private int displayPageNum;
+    private PageCriteria criteria;
+
+    public PageMaker(PageCriteria criteria, Integer totalCount) {
+        checkNotNull(criteria, "페이징 설정 기준은 null이 될 수 없습니다.");
+        checkArgument(totalCount != null && totalCount >= 1, "총 데이터의 수는 필수이며 음수가 될 수 없습니다.");
+
+        this.criteria = criteria;
+        this.totalCount = totalCount;
+        displayPageNum = ObjectUtils.defaultIfNull(criteria.getPerPageNum(), 10);
+        calcData();
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public int getStartPage() {
+        return startPage;
+    }
+
+    public int getEndPage() {
+        return endPage;
+    }
+
+    public boolean isPrev() {
+        return prev;
+    }
+
+    public boolean isNext() {
+        return next;
+    }
+
+    public int getDisplayPageNum() {
+        return displayPageNum;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("totalCount", totalCount)
+                .append("startPage", startPage)
+                .append("endPage", endPage)
+                .append("prev", prev)
+                .append("next", next)
+                .append("displayPageNum", displayPageNum)
+                .toString();
+    }
+
+    private void calcData() {
+        // 현재 페이지를 기준으로 마지막 페이지 번호 계산
+        // (현재 페이지 번호 / 페이지 번호의 수) * 보여지는 페이지 번호 수
+        endPage = (int) (Math.ceil(criteria.getPage() / (double) displayPageNum)
+                * displayPageNum);
+
+        // (마지막 페이지 번호 - 보여지는 페이지 번호 수) + 1
+        startPage = (endPage - displayPageNum) + 1;
+
+        // 데이터 수를 기준으로 마지막 페이지 번호 계산
+        int tempEndPage = (int) Math.ceil(totalCount / (double) displayPageNum);
+
+        // 보여져야 할 번호 수에 필요한 데이터보다 총 데이터 수가 모자르다면 마지막 페이지 번호를 조정
+        if (endPage > tempEndPage){
+            endPage = tempEndPage;
+        }
+
+        // 1 = false, 그 외는 true
+        prev = startPage != 1;
+
+        // 뒤에 데이터가 더 남아 있다면 true, 적다면 false
+        next = endPage * displayPageNum < totalCount;
+    }
+}
--- src/test/java/net/gentledot/springcodeproject/model/board/BoardTest.java
+++ src/test/java/net/gentledot/springcodeproject/model/board/BoardTest.java
@@ -14,7 +14,7 @@ class BoardTest {
 
     @Test
     @DisplayName("Board 객체 생성 테스트")
-    void createBoardTest(){
+    void createBoardTest() {
         String title = "테스트타이틀";
         String content = "안녕하세요. 테스트 내용입니다.";
         String username = "testUser01";
@@ -36,7 +36,7 @@ class BoardTest {
 
     @Test
     @DisplayName("Board 객체 생성 후 view count 증가 테스트")
-    void createBoardTestWithIncreaseViewCnt(){
+    void createBoardTestWithIncreaseViewCnt() {
         String title = "테스트타이틀";
         String content = "안녕하세요. 테스트 내용입니다.";
         String username = "testUser01";
@@ -59,5 +59,18 @@ class BoardTest {
         assertThat(newBoard.getViewcnt(), is(viewcnt));
 
         log.debug("생성된 객체 : {}", newBoard);
+    }
+
+    @Test
+    @DisplayName("페이지 처리용 클래스 생성 테스트")
+    void createPageMakerTest() {
+        // 10개씩 데이터 출력, totalCount = 122개 일때
+        // page = 3인 경우 : totalCount=122,startPage=1,endPage=10,prev=false,next=true,displayPageNum=10
+        // page = 10인 경우 : totalCount=122,startPage=1,endPage=10,prev=false,next=true,displayPageNum=10
+        // page가 11인 경우 : totalCount=122,startPage=11,endPage=13,prev=true,next=false,displayPageNum=10
+        PageCriteria pageCriteria = new PageCriteria(11, 10);
+        PageMaker pageMaker = new PageMaker(pageCriteria, 122);
+
+        log.debug("생성된 페이지 처리용 클래스 : {}", pageMaker);
     }
 }
